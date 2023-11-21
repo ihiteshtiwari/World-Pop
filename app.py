4c9e9af2deb75b935fd8c2fc1ef8e618687fd21c
Hitesh Kumar Tiwari <122075432+ihiteshtiwari@users.noreply.github.com>
2023-11-21 11:10:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ option = st.sidebar.selectbox("Select an option", ("Home", "Choropleth Map", "Ba
 
 if option == "Home":
     st.title("  ")
-    st.write("" ,data)
+    st.write("" ,data,"/n ref: https://www.kaggle.com/datasets/iamsouravbanerjee/world-population-dataset/data")
     
     # Read the contents of the HTML file
     with open("index.html") as file:
